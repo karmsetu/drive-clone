b099c63322d6d4f24c558fdbdbd3e0b116c01dac
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-12-14 01:50:33 +0530

    <Card />

---
+++ src/app/(root)/[type]/page.tsx
@@ -0,0 +1,42 @@
+import Card from '@/components/Card';
+import Sort from '@/components/Sort';
+import { getFiles } from '@/lib/actions/file.actions';
+
+const page = async ({ params }: SearchParamProps) => {
+    const type = ((await params).type as string) || '';
+
+    const files = await getFiles();
+
+    return (
+        <div className="page-container">
+            <section className="w-full ">
+                <h1 className="h1 capitalize">{type}</h1>
+                <div className="total-size-section">
+                    <p className="body-1">
+                        Total: <span className="h-5">0mb</span>
+                    </p>
+
+                    <div className="sort-container">
+                        <p className="body-1 hidden sm:block text-light-200">
+                            Sort by:
+                        </p>
+                        <Sort />
+                    </div>
+                </div>
+            </section>
+
+            {/* Render the files */}
+            {files.total > 0 ? (
+                <section className="file-list">
+                    {files.documents.map((file) => (
+                        <Card key={`${file.$id}`} file={file} />
+                    ))}
+                </section>
+            ) : (
+                <p className="empty-list">No files uploaded</p>
+            )}
+        </div>
+    );
+};
+
+export default page;
