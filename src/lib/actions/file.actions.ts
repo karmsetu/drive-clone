e61c21779852111ab4f6721d3028ec6d5c33dcb6
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-12-14 01:33:02 +0530

    getFiles - queryless

--- src/lib/actions/file.actions.ts
+++ src/lib/actions/file.actions.ts
@@ -3,9 +3,10 @@
 import { createAdminClient } from '../appwrite';
 import { InputFile } from 'node-appwrite/file';
 import { appwriteConfig } from '../appwrite/config';
-import { ID } from 'node-appwrite';
+import { ID, Models, Query } from 'node-appwrite';
 import { constructFileUrl, getFileType, parseStringify } from '../utils';
 import { revalidatePath } from 'next/cache';
+import { getCurrentUser } from './user.actions';
 
 const handleError = (error: unknown, message: string) => {
     console.log(error, message);
@@ -60,4 +61,37 @@ export const uploadFile = async ({
     } catch (error) {
         handleError(error, 'Failed to upload files');
     }
+};
+
+export const getFiles = async () => {
+    const { databases } = await createAdminClient();
+    try {
+        const currentUser = await getCurrentUser();
+
+        if (!currentUser) {
+            throw Error('user not found');
+        }
+
+        const queries = await createQueries(currentUser);
+        const files = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            queries
+        );
+
+        return parseStringify(files);
+    } catch (error) {
+        handleError(error, 'failed to get files');
+    }
+};
+
+const createQueries = async (currentUser: Models.Document) => {
+    const queries = [
+        Query.or([
+            Query.equal('owner', [currentUser.$id]),
+            Query.contains('users', [currentUser.email]),
+        ]),
+    ];
+
+    return queries;
 };
